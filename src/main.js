6cc7aa6e7076dd875994a89843fcbf62c9361852
oychao <chao@ouyang.io>
2019-05-19 01:59:10 +0800

    [feature] manage keys

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-export default function (world) {
-  return `hello ${world}`;
+const DICT = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';
+const DICT_LEN = DICT.length;
+
+const store = new WeakMap();
+
+export default function (item, keyLen = 10) {
+  if (typeof item !== 'object') {
+    throw new Error(`item of list should be an object while a ${typeof item} is given`);
+  }
+  if (store.has(item)) {
+    return store.get(item);
+  }
+  const ret = [];
+  const legalLen = keyLen < 10 ? 10 : keyLen;
+  let i;
+  for (i = 0; i < legalLen; i += 1) {
+    ret.push(Math.floor(Math.random() * DICT_LEN));
+  }
+  const str = ret.join('');
+  store.set(item, str);
+  return str;
 }
