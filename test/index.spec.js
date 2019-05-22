eae3ccc11553d0780dfb6c09aa2962a7c555fd75
oychao <chao@ouyang.io>
2019-05-22 23:37:41 +0800

    [test] add some test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -20,8 +20,27 @@ describe('key-defender', () => {
   });
 
   it('should only accept an object as its first parameter', (done) => {
-    const testFn = function () { k('str'); };
-    expect(testFn).to.throw();
+    expect(function () { k(undefined); }).to.throw();
+    expect(function () { k(null); }).to.throw();
+    expect(function () { k(123); }).to.throw();
+    expect(function () { k('str'); }).to.throw();
+    done();
+  });
+
+  it('function and array could be passed as its first parameter', (done) => {
+    expect(typeof k(() => {})).to.equal('string');
+    expect(typeof k([])).to.equal('string');
+    done();
+  });
+
+  it('should return same key strings if identical object passed', (done) => {
+    const obj = {};
+    expect(k(obj)).to.equal(k(obj));
+    done();
+  });
+
+  it('should return different key strings if same valued object passed', (done) => {
+    expect(k({})).to.not.equal(k({}));
     done();
   });
 });
